172d41d53aa62488b98019cc14b8f4d10b2937ec
Daniel <46411530+DanielEliasib@users.noreply.github.com>
2024-07-23 11:22:36 -0600

    test commit

--- lua/plugins/todo-comments-nvim.lua
+++ lua/plugins/todo-comments-nvim.lua
@@ -2,7 +2,6 @@ return {
 	'folke/todo-comments.nvim',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	opts = {
-
 		signs = true, -- show icons in the signs column
 		-- keywords recognized as todo comments
 		keywords = {
