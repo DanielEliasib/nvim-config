1c0027b70c5de2d5ef935f1e6b5acfa175e511f8
Daniel <46411530+DanielEliasib@users.noreply.github.com>
2024-08-11 19:52:18 -0600

    remove codecompanion
    - restore copilot chat
    - add full lsp diagnostic command to copilo

--- lua/plugins/CopilotChat-nvim.lua
+++ lua/plugins/CopilotChat-nvim.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
 		branch = "canary",
-		enabled = false,
+		enabled = true,
 		dependencies = {
 			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
 			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
@@ -21,8 +21,40 @@ return {
 				height = 0.45, -- fractional height of parent, or absolute height in rows when > 1
 				title = 'Chat', -- title of chat window
 				relative = 'editor', -- 'editor', 'win', 'cursor', 'mouse'
-				
 
+
+			},
+			prompts = {
+				Diagnostics = {
+					prompt = '/COPILOT_GENERATE Provide solutions to the following diagnostics:',
+					selection = function(source)
+						local bufnr = source.bufnr
+						local select = require('CopilotChat.select')
+						local select_buffer = select.buffer(source)
+						local winnr = source.winnr
+						if not select_buffer then
+							return nil
+						end
+						local cursor = vim.api.nvim_win_get_cursor(winnr)
+
+						file_diagnositcs = vim.diagnostic.get(bufnr,
+							{ severity = { min = vim.diagnostic.severity.HINT } })
+						if #file_diagnositcs == 0 or file_diagnositcs == nil then
+							return nil
+						end
+						local diagnostics = {}
+						for _, diagnostic in ipairs(file_diagnositcs) do
+							table.insert(diagnostics, diagnostic.message)
+						end
+
+						local result = table.concat(diagnostics, '. ')
+						result = result:gsub('^%s*(.-)%s*$', '%1'):gsub('\n', ' ')
+
+						local file_name = vim.api.nvim_buf_get_name(bufnr)
+						select_buffer.prompt_extra = file_name .. ':' .. cursor[1] .. '. ' .. result
+						return select_buffer
+					end
+				}
 			},
 
 			-- window = {
@@ -41,7 +73,7 @@ return {
 			error_header = '»» Error ', -- Header to use for errors
 		},
 		keys = {
-			{ '<leader>ct', '<cmd>CopilotChatToggle<CR>', mode = { "n", "v" }, silent = true, nowait = true, noremap = true, desc = 'Toggle Copilot Chat' },
+			{ '<leader>cc', '<cmd>CopilotChatToggle<CR>', mode = { "n", "v" }, silent = true, nowait = true, noremap = true, desc = 'Toggle Copilot Chat' },
 		}
 		-- See Commands section for default commands if you want to lazy load on them
 	},
